056583c85f2a49f6eb9558359471d46d0456aa09
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2023-08-27 13:53:58 +0100

    constituency active scope

--- app/models/constituency.rb
+++ app/models/constituency.rb
@@ -2,7 +2,10 @@ class Constituency < ApplicationRecord
   belongs_to :member
   has_many :election_results
 
+  scope :active, -> { where(end_date: nil) }
+
   def latest_election 
     election_results.order( election_date: :desc).first
   end
+
 end
